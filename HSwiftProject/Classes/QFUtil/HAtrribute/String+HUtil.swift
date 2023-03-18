8ac7de992bb2d7258f54673a545ed440dd251d50
dqf <897432098@qq.com>
2023-03-18 14:31:39 +0800

    modify some files

---
+++ HSwiftProject/Classes/QFUtil/HAtrribute/String+HUtil.swift
@@ -0,0 +1,16 @@
+//
+//  String+HUtil.swift
+//  HSwiftProject
+//
+//  Created by Wind on 17/11/2021.
+//  Copyright © 2021 wind. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func attribute() -> NSMutableAttributedString {
+        return NSMutableAttributedString(string: self)
+    }
+}
+
