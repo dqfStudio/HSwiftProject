655bd27bdd67e7855201fddd22a5ad12b42614b9
dqf <897432098@qq.com>
2023-04-29 12:40:34 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/NSIndexPath+HUtil.swift
+++ HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/NSIndexPath+HUtil.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 extension NSIndexPath {
-    var stringValue: NSString {
+    var stringValue: String {
+        return "\(self.row)" + "\(self.section)"
+    }
+    var nsStringValue: NSString {
         return "\(self.row)" + "\(self.section)" as NSString
     }
     
-    static func stringValue(_ row: Int, _ section: Int) -> NSString {
+    static func stringValue(_ row: Int, _ section: Int) -> String {
+        return "\(row)" + "\(section)"
+    }
+    static func nsStringValue(_ row: Int, _ section: Int) -> NSString {
         return "\(row)" + "\(section)" as NSString
     }
+    
     static func instanceValue(_ row: Int, _ section: Int) -> NSIndexPath {
         return NSIndexPath(row: row, section: section)
     }
