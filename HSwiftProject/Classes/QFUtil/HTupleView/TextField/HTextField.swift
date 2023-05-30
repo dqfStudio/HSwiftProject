591959a11796221f5dbdca2bec86840958a0f892
dqf <897432098@qq.com>
2023-05-30 21:50:10 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HTupleView/TextField/HTextField.swift
+++ HSwiftProject/Classes/QFUtil/HTupleView/TextField/HTextField.swift
@@ -46,7 +46,7 @@ class HTextField : UITextField, UITextFieldDelegate {
         return imageView
     }()
 
-    lazy var leftItem: HWebButtonView = {
+    lazy var leftleftButton: HWebButtonView = {
         let button = HWebButtonView()
         button.textFont = UIFont.systemFont(ofSize: 14)
         self.leftViewMode = .always
