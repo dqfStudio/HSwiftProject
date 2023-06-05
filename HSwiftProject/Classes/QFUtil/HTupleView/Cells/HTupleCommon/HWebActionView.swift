9baa93180aedae0bfc67798039b66f6cc2eb3e5a
dqf <897432098@qq.com>
2023-06-05 16:26:56 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HWebActionView.swift
+++ HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HWebActionView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 enum HActionDirection: Int {
     case horizontal = 0 // Horizontal design
@@ -67,6 +68,44 @@ class HWebActionView: UIControl {
         return layoutView
     }()
     
+    private var lastURL: String = ""
+    // Click time
+    private var pressedInterval: TimeInterval = 0.0
+    
+    // The tintColor in the parent class is problematic
+    var renderColor: UIColor? {
+        didSet {
+            guard let color = renderColor else {
+                imageView.image = imageView.image?.withRenderingMode(.alwaysOriginal)
+                return
+            }
+            imageView.tintColor = color
+            imageView.image = imageView.image?.withRenderingMode(.alwaysTemplate)
+        }
+    }
+    
+    var hasImage: Bool {
+        return _imageView?.image != nil
+    }
+    var pressed: Callback?
+    var didGetImage: Callback?
+    var didGetError: Callback?
+    
+    required init() {
+        super.init(frame: .zero)
+        self.setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setup()
+    }
+    
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
     override var frame: CGRect {
         didSet {
             guard frame != oldValue else { return }
@@ -75,14 +114,26 @@ class HWebActionView: UIControl {
             layoutView.frame = _frame
         }
     }
+
+    private func setup() {
+        self.addTarget(self, action: #selector(buttonPressed), for:.touchUpInside)
+    }
     
+    // Click response event
+    @objc
+    private func buttonPressed() {
+        guard let pressed = pressed else { return }
+        // Click time
+        if Date().timeIntervalSince1970 - pressedInterval > 0.5 {
+            // Record click time
+            pressedInterval = Date().timeIntervalSince1970
+            // Callback
+            pressed(self, nil)
+        }
+    }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        setup()
-    }
-    
-    private func setup() {
         
         /// 左边布局View
         layoutView.addArrangedSubview(leftView)
@@ -192,6 +243,167 @@ class HWebActionView: UIControl {
         /// 右边布局View
         layoutView.addArrangedSubview(rightView)
         
+    }
+    
+}
+
+extension HWebActionView {
+
+    private func _setImage(_ image: UIImage?) {
+        imageView.kf.cancelDownloadTask()
+        guard let image = image else {
+            imageView.image = nil
+            return
+        }
+        
+        if let renderColor = renderColor {
+            imageView.tintColor = renderColor
+            imageView.image = image.withRenderingMode(.alwaysTemplate)
+        } else {
+            imageView.image = image
+        }
+    }
+    
+    /**
+    *  Set image directly
+    *
+    *  @param image image
+    */
+    func setImage(_ image: UIImage?) {
+        self._setImage(image)
+        self.lastURL = ""
+        self.alpha = 1
+        didGetImage?(self, image!)
+    }
+
+    /**
+    *  Set image link, read cache synchronously if available
+    *
+    *  @param url                      Link
+    *  @param placeholder        Placeholder image
+    *  @param syncLoadCache Synchronously read cache
+    *
+    */
+    func setImageUrl(_ url: URL, placeholder: UIImage? = nil, syncLoadCache cache: Bool = true) {
+        self.setImageUrlString(url.absoluteString, placeholder: placeholder, syncLoadCache: cache)
+    }
+    
+    /**
+    *  Set image link, read from cache if available
+    *
+    *  @param urlString             link string
+    *  @param placeholder        default image
+    *  @param syncLoadCache whether to read cache synchronously
+    *
+    */
+    func setImageUrlString(_ urlString: String, placeholder: UIImage? = nil, syncLoadCache cache: Bool = true) {
+        if urlString.count == 0 {
+            self._setImage(nil)
+            self.lastURL = ""
+            if didGetError != nil {
+                didGetError!(self, herr(kDataFormatErrorCode, desc: "url = \(urlString)"))
+            }
+            return
+        }
+        
+        if urlString.hasPrefix("http") == false {
+            let image: UIImage? = UIImage(named: urlString)
+            self._setImage(image)
+            self.alpha = 1
+            if didGetImage != nil {
+                didGetImage!(self, imageView.image!)
+            }
+            return
+        }
+        if imageView.image != nil && lastURL.isEqual(urlString) {
+            self.alpha = 1
+            if didGetImage != nil {
+                didGetImage!(self, imageView.image!)
+            }
+            return
+        }
+        
+        if placeholder == nil && imageView.image == nil {
+            self.alpha = 0
+        }
+        
+        self._setImage(nil)
+        self.lastURL = ""
+        let url: URL = URL(string: urlString)!
+        
+        if cache {
+            KingfisherManager.shared.cache.retrieveImage(forKey: urlString) { result in
+                switch result {
+                case.success(let value):
+                    if value.image != nil {
+                        self._setImage(value.image)
+                        self.alpha = 1
+                        self.lastURL = url.absoluteString
+                        if self.didGetImage != nil {
+                            self.didGetImage!(self, value.image)
+                        }
+                    }
+                case .failure(_): break
+                }
+            }
+        }
+        if imageView.image == nil {
+            //self.kf.indicatorType = .activity
+            imageView.kf.setImage(with: url, placeholder: placeholder, options: [.transition(ImageTransition.fade(1))], progressBlock: nil) { result in
+                switch result {
+                case .success(let value):
+                    self._setImage(value.image)
+                    self.lastURL = url.absoluteString
+                    if value.cacheType == .none {
+                        UIView.animate(withDuration: 0.5) {
+                            self.alpha = 1
+                        }
+                    }else {
+                        self.alpha = 1
+                    }
+                    if self.didGetImage != nil {
+                        self.didGetImage!(self, value.image)
+                    }
+                case .failure(let value):
+                    if self.didGetError != nil {
+                        self.didGetError!(self, value as AnyObject)
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+    *  Set the image name and load it through the file
+    *
+    *  @param fileName image name
+    */
+    func setImageWithFile(_ fileName: String) {
+        if fileName.count > 0 {
+            if let resourcePath: String = Bundle.main.resourcePath {
+                let filePath: String = resourcePath.appendingFormat("/%@", fileName)
+                if let image: UIImage = UIImage(contentsOfFile: filePath) {
+                    self.setImage(image)
+                }
+            }
+        }else {
+            self._setImage(nil)
+            self.lastURL = ""
+            if didGetError != nil {
+                didGetError!(self, herr(kDataFormatErrorCode, desc: "url = \(fileName)"))
+            }
+        }
+    }
+
+    /**
+    *  Set the image name and load it using imageName
+    *
+    *  @param fileName The name of the image
+    */
+    func setImageWithName(_ fileName: String) {
+        if fileName.count > 0 {
+            self.setImage(UIImage(named: fileName))
+        }
     }
     
 }
