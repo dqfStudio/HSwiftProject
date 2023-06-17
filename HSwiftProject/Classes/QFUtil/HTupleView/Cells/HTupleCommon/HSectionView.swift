a0b246b46977a7c70d16e2f536c47b2270875f74
dqf <897432098@qq.com>
2023-06-17 17:09:35 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HSectionView.swift
+++ HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HSectionView.swift
@@ -6,73 +6,73 @@
 //  Copyright © 2023 wind. All rights reserved.
 //
 
-import UIKit
-
-class HSectionView: UIStackView {
-    
-    private var allReuseViews = NSMapTable<NSString, AnyObject>.strongToStrongObjects()
-    
-    var header: UIView? {
-        get { return allReuseViews.object(forKey: "header") as? UIView }
-        set { allReuseViews.setObject(newValue, forKey: "header") }
-    }
-    var footer: UIView? {
-        get { return allReuseViews.object(forKey: "footer") as? UIView }
-        set { allReuseViews.setObject(newValue, forKey: "footer") }
-    }
-    var item: UIView? {
-        get { return allReuseViews.object(forKey: "item") as? UIView }
-        set { allReuseViews.setObject(newValue, forKey: "item") }
-    }
-
-    var headerWidth = 0.0
-    var footerWidth = 0.0
-
-    var headerSpace = 0.0
-    var footerSpace = 0.0
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        setup()
-    }
-    
-    private func setup() {
-        
-        if let item = item {
-            
-            // header
-            if let header = header {
-                if self.axis == .vertical {
-                    header.heightAnchor.constraint(equalToConstant: headerWidth).isActive = true
-                } else {
-                    header.widthAnchor.constraint(equalToConstant: headerWidth).isActive = true
-                }
-                self.addArrangedSubview(header)
-                if headerSpace > 0 {
-                    self.setCustomSpacing(headerSpace, after: header)
-                }
-            }
-            
-            // item
-            self.addArrangedSubview(item)
-            
-            // footer
-            if let footer = footer {
-                if self.axis == .vertical {
-                    footer.heightAnchor.constraint(equalToConstant: footerWidth).isActive = true
-                } else {
-                    footer.widthAnchor.constraint(equalToConstant: footerWidth).isActive = true
-                }
-                self.addArrangedSubview(footer)
-                if footerSpace > 0 {
-                    self.setCustomSpacing(footerSpace, after: item)
-                }
-                
-            }
-            
-        }
-        
-    }
-    
-}
-
+//import UIKit
+//
+//class HSectionView: UIStackView {
+//
+//    private var allReuseViews = NSMapTable<NSString, AnyObject>.strongToStrongObjects()
+//
+//    var header: UIView? {
+//        get { return allReuseViews.object(forKey: "header") as? UIView }
+//        set { allReuseViews.setObject(newValue, forKey: "header") }
+//    }
+//    var footer: UIView? {
+//        get { return allReuseViews.object(forKey: "footer") as? UIView }
+//        set { allReuseViews.setObject(newValue, forKey: "footer") }
+//    }
+//    var item: UIView? {
+//        get { return allReuseViews.object(forKey: "item") as? UIView }
+//        set { allReuseViews.setObject(newValue, forKey: "item") }
+//    }
+//
+//    var headerWidth = 0.0
+//    var footerWidth = 0.0
+//
+//    var headerSpace = 0.0
+//    var footerSpace = 0.0
+//
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        setup()
+//    }
+//
+//    private func setup() {
+//
+//        if let item = item {
+//
+//            // header
+//            if let header = header {
+//                if self.axis == .vertical {
+//                    header.heightAnchor.constraint(equalToConstant: headerWidth).isActive = true
+//                } else {
+//                    header.widthAnchor.constraint(equalToConstant: headerWidth).isActive = true
+//                }
+//                self.addArrangedSubview(header)
+//                if headerSpace > 0 {
+//                    self.setCustomSpacing(headerSpace, after: header)
+//                }
+//            }
+//
+//            // item
+//            self.addArrangedSubview(item)
+//
+//            // footer
+//            if let footer = footer {
+//                if self.axis == .vertical {
+//                    footer.heightAnchor.constraint(equalToConstant: footerWidth).isActive = true
+//                } else {
+//                    footer.widthAnchor.constraint(equalToConstant: footerWidth).isActive = true
+//                }
+//                self.addArrangedSubview(footer)
+//                if footerSpace > 0 {
+//                    self.setCustomSpacing(footerSpace, after: item)
+//                }
+//
+//            }
+//
+//        }
+//
+//    }
+//
+//}
+//
--- HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HWebActionView.swift
+++ HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HWebActionView.swift
@@ -6,260 +6,260 @@
 //  Copyright © 2023 wind. All rights reserved.
 //
 
-import UIKit
-import Kingfisher
-
-class HWebActionView: UIButton {
-
-    enum Position {
-        case top
-        case left
-        case bottom
-        case right
-    }
-
-    var imageSpace: CGFloat = 0.0
-    var imageSize: CGSize = .zero
-    var imagePosition: Position = .left
-    var hasImage: Bool {
-        return self.imageView?.image != nil
-    }
-    
-    var pressed: Callback?
-    var didGetImage: Callback?
-    var didGetError: Callback?
-    
-    private var originBounds: CGRect = .zero
-    private var needRefresh: Bool = true
-
-    private var lastURL: String = ""
-    // Click time
-    private var pressedInterval: TimeInterval = 0.0
-
-    // The tintColor in the parent class is problematic
-    var renderColor: UIColor? {
-        didSet {
-            guard let color = renderColor else {
-                self.setImage(self.image(for:.normal)?.withRenderingMode(.alwaysOriginal), for:.normal)
-                return
-            }
-            self.imageView?.tintColor = color
-            self.setImage(self.image(for:.normal)?.withRenderingMode(.alwaysTemplate), for:.normal)
-        }
-    }
-
-    required init() {
-        super.init(frame: .zero)
-        self.setup()
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        self.setup()
-    }
-
-    required override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.setup()
-    }
-
-    private func setup() {
-        self.backgroundColor = .clear
-        self.imageView?.contentMode = .scaleAspectFill
-        self.titleLabel?.font = .systemFont(ofSize: 17.0)
-        self.layer.masksToBounds = true
-        self.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-    }
-
-    // Click response event
-    @objc
-    private func buttonPressed() {
-        guard let pressed = pressed else { return }
-        // Click time
-        if Date().timeIntervalSince1970 - pressedInterval > 0.5 {
-            // Record click time
-            pressedInterval = Date().timeIntervalSince1970
-            // Callback
-            pressed(self, nil)
-        }
-    }
-
-    override var intrinsicContentSize: CGSize {
-        var size = super.intrinsicContentSize
-        switch imagePosition {
-        case .left, .right:
-            size.width += imageSpace
-        case .top, .bottom:
-            let titleHeight = titleLabel?.bounds.height ?? 0
-            let imageHeight = imageView?.bounds.height ?? 0
-            size.height = imageSpace + titleHeight + imageHeight
-        }
-        return size
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        if self.superview != nil {
-            needRefresh = true //需要刷新
-        }
-        if originBounds != bounds {
-            originBounds = bounds
-            needRefresh = true //需要刷新
-        }
-        if needRefresh {
-            needRefresh = false
-            updatePosition()
-        }
-    }
-
-    private func updatePosition() {
-        switch imagePosition {
-        case .top:
-            let imageWidth = imageView?.bounds.width ?? 0
-            let titleWidth = titleLabel?.bounds.width ?? 0
-            let titleHeight = titleLabel?.bounds.height ?? 0
-            let imageHeight = imageView?.bounds.height ?? 0
-            titleEdgeInsets = UIEdgeInsets(top: (titleHeight + imageSpace) * 0.5, left: -imageWidth * 0.5, bottom: -imageSpace, right: imageWidth * 0.5)
-            imageEdgeInsets = UIEdgeInsets(top: 0, left: titleWidth * 0.5, bottom: imageHeight + imageSpace, right: -titleWidth * 0.5)
-        case .left:
-            titleEdgeInsets = UIEdgeInsets(top: 0, left: imageSpace, bottom: 0, right: 0)
-            imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: imageSpace)
-        case .bottom:
-            let imageWidth = imageView?.bounds.width ?? 0
-            let titleWidth = titleLabel?.bounds.width ?? 0
-            let titleHeight = titleLabel?.bounds.height ?? 0
-            let imageHeight = imageView?.bounds.height ?? 0
-            titleEdgeInsets = UIEdgeInsets(top: -(titleHeight + imageSpace) * 0.5, left: -imageWidth * 0.5, bottom: imageSpace, right: imageWidth * 0.5)
-            imageEdgeInsets = UIEdgeInsets(top: imageHeight + imageSpace, left: titleWidth * 0.5, bottom: 0, right: -titleWidth * 0.5)
-        case .right:
-            let imageWidth = (imageView?.bounds.width ?? 0) + imageSpace * 0.5
-            let titleWidth = (titleLabel?.bounds.width ?? 0) + imageSpace * 0.5
-            titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
-            imageEdgeInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: 0, right: -titleWidth)
-        }
-    }
-
-}
-
-
-extension HWebActionView {
-
-    private func _setImage(_ image: UIImage?) {
-        self.imageView?.kf.cancelDownloadTask()
-        guard let image = image else {
-            self.imageView?.image = nil
-            return
-        }
-        if let renderColor = renderColor {
-            self.imageView?.tintColor = renderColor
-            if imageSize != .zero {
-                let image = image.withRenderingMode(.alwaysTemplate)
-                self.setImage(image.scaleImage(imageSize), for: .normal)
-            } else {
-                self.setImage(image.withRenderingMode(.alwaysTemplate), for: .normal)
-            }
-        } else {
-            if imageSize != .zero {
-                self.setImage(image.scaleImage(imageSize), for: .normal)
-            } else {
-                self.setImage(image, for: .normal)
-            }
-        }
-        // 需要刷新
-        needRefresh = true
-    }
-
-    /**
-    *  Set image link, read cache synchronously if available
-    *
-    *  @param url                      Link
-    *  @param placeholder        Placeholder image
-    *  @param syncLoadCache Synchronously read cache
-    *
-    */
-    func setImageUrl(_ url: URL, placeholder: UIImage? = nil, syncLoadCache cache: Bool = true) {
-        self.setImageUrlString(url.absoluteString, placeholder: placeholder, syncLoadCache: cache)
-    }
-
-    /**
-    *  Set image link, read from cache if available
-    *
-    *  @param urlString             link string
-    *  @param placeholder        default image
-    *  @param syncLoadCache whether to read cache synchronously
-    *
-    */
-    func setImageUrlString(_ urlString: String, placeholder: UIImage? = nil, syncLoadCache cache: Bool = true) {
-        if urlString.count == 0 {
-            self._setImage(placeholder)
-            self.lastURL = ""
-            didGetError?(self, herr(kDataFormatErrorCode, desc: "url = \(urlString)"))
-            return
-        }
-
-        if urlString.hasPrefix("http") == false {
-            let image = UIImage(named: urlString)
-            self._setImage(image)
-            self.imageView?.alpha = 1.0
-            didGetImage?(self, self.imageView?.image)
-            return
-        }
-
-        if self.imageView?.image != nil, lastURL.isEqual(urlString) {
-            self.imageView?.alpha = 1.0
-            didGetImage?(self, self.imageView?.image)
-            return
-        }
-
-        if self.imageView?.image == nil, placeholder == nil {
-            self.imageView?.alpha = 0
-        }
-
-        self._setImage(nil)
-        self.lastURL = ""
-        
-        guard let url = URL(string: urlString) else {
-            return
-        }
-
-        if cache {
-            KingfisherManager.shared.cache.retrieveImage(forKey: urlString) { result in
-                DispatchQueue.main.async {
-                    switch result {
-                    case.success(let value):
-                        if value.image != nil {
-                            self._setImage(value.image)
-                            self.imageView?.alpha = 1.0
-                            self.lastURL = url.absoluteString
-                            self.didGetImage?(self, value.image)
-                        }
-                    case .failure(_): break
-                    }
-                }
-            }
-        }
-        if self.imageView?.image == nil {
-            //self.imageView?.kf.indicatorType = .activity
-            self.imageView?.kf.setImage(with: url, placeholder: placeholder, options: [.transition(ImageTransition.fade(1))], progressBlock: nil) { result in
-                DispatchQueue.main.async {
-                    switch result {
-                    case .success(let value):
-                        self._setImage(value.image)
-                        self.lastURL = url.absoluteString
-                        if value.cacheType == .none {
-                            UIView.animate(withDuration: 0.25) {
-                                self.imageView?.alpha = 1.0
-                            }
-                        }else {
-                            self.imageView?.alpha = 1.0
-                        }
-                        self.didGetImage?(self, value.image)
-                    case .failure(let value):
-                        self.didGetError?(self, value)
-                    }
-                }
-            }
-        }
-    }
-
-}
+//import UIKit
+//import Kingfisher
+//
+//class HWebActionView: UIButton {
+//
+//    enum Position {
+//        case top
+//        case left
+//        case bottom
+//        case right
+//    }
+//
+//    var imageSpace: CGFloat = 0.0
+//    var imageSize: CGSize = .zero
+//    var imagePosition: Position = .left
+//    var hasImage: Bool {
+//        return self.imageView?.image != nil
+//    }
+//    
+//    var pressed: Callback?
+//    var didGetImage: Callback?
+//    var didGetError: Callback?
+//    
+//    private var originBounds: CGRect = .zero
+//    private var needRefresh: Bool = true
+//
+//    private var lastURL: String = ""
+//    // Click time
+//    private var pressedInterval: TimeInterval = 0.0
+//
+//    // The tintColor in the parent class is problematic
+//    var renderColor: UIColor? {
+//        didSet {
+//            guard let color = renderColor else {
+//                self.setImage(self.image(for:.normal)?.withRenderingMode(.alwaysOriginal), for:.normal)
+//                return
+//            }
+//            self.imageView?.tintColor = color
+//            self.setImage(self.image(for:.normal)?.withRenderingMode(.alwaysTemplate), for:.normal)
+//        }
+//    }
+//
+//    required init() {
+//        super.init(frame: .zero)
+//        self.setup()
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        self.setup()
+//    }
+//
+//    required override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        self.setup()
+//    }
+//
+//    private func setup() {
+//        self.backgroundColor = .clear
+//        self.imageView?.contentMode = .scaleAspectFill
+//        self.titleLabel?.font = .systemFont(ofSize: 17.0)
+//        self.layer.masksToBounds = true
+//        self.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+//    }
+//
+//    // Click response event
+//    @objc
+//    private func buttonPressed() {
+//        guard let pressed = pressed else { return }
+//        // Click time
+//        if Date().timeIntervalSince1970 - pressedInterval > 0.5 {
+//            // Record click time
+//            pressedInterval = Date().timeIntervalSince1970
+//            // Callback
+//            pressed(self, nil)
+//        }
+//    }
+//
+//    override var intrinsicContentSize: CGSize {
+//        var size = super.intrinsicContentSize
+//        switch imagePosition {
+//        case .left, .right:
+//            size.width += imageSpace
+//        case .top, .bottom:
+//            let titleHeight = titleLabel?.bounds.height ?? 0
+//            let imageHeight = imageView?.bounds.height ?? 0
+//            size.height = imageSpace + titleHeight + imageHeight
+//        }
+//        return size
+//    }
+//
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        if self.superview != nil {
+//            needRefresh = true //需要刷新
+//        }
+//        if originBounds != bounds {
+//            originBounds = bounds
+//            needRefresh = true //需要刷新
+//        }
+//        if needRefresh {
+//            needRefresh = false
+//            updatePosition()
+//        }
+//    }
+//
+//    private func updatePosition() {
+//        switch imagePosition {
+//        case .top:
+//            let imageWidth = imageView?.bounds.width ?? 0
+//            let titleWidth = titleLabel?.bounds.width ?? 0
+//            let titleHeight = titleLabel?.bounds.height ?? 0
+//            let imageHeight = imageView?.bounds.height ?? 0
+//            titleEdgeInsets = UIEdgeInsets(top: (titleHeight + imageSpace) * 0.5, left: -imageWidth * 0.5, bottom: -imageSpace, right: imageWidth * 0.5)
+//            imageEdgeInsets = UIEdgeInsets(top: 0, left: titleWidth * 0.5, bottom: imageHeight + imageSpace, right: -titleWidth * 0.5)
+//        case .left:
+//            titleEdgeInsets = UIEdgeInsets(top: 0, left: imageSpace, bottom: 0, right: 0)
+//            imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: imageSpace)
+//        case .bottom:
+//            let imageWidth = imageView?.bounds.width ?? 0
+//            let titleWidth = titleLabel?.bounds.width ?? 0
+//            let titleHeight = titleLabel?.bounds.height ?? 0
+//            let imageHeight = imageView?.bounds.height ?? 0
+//            titleEdgeInsets = UIEdgeInsets(top: -(titleHeight + imageSpace) * 0.5, left: -imageWidth * 0.5, bottom: imageSpace, right: imageWidth * 0.5)
+//            imageEdgeInsets = UIEdgeInsets(top: imageHeight + imageSpace, left: titleWidth * 0.5, bottom: 0, right: -titleWidth * 0.5)
+//        case .right:
+//            let imageWidth = (imageView?.bounds.width ?? 0) + imageSpace * 0.5
+//            let titleWidth = (titleLabel?.bounds.width ?? 0) + imageSpace * 0.5
+//            titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
+//            imageEdgeInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: 0, right: -titleWidth)
+//        }
+//    }
+//
+//}
+//
+//
+//extension HWebActionView {
+//
+//    private func _setImage(_ image: UIImage?) {
+//        self.imageView?.kf.cancelDownloadTask()
+//        guard let image = image else {
+//            self.imageView?.image = nil
+//            return
+//        }
+//        if let renderColor = renderColor {
+//            self.imageView?.tintColor = renderColor
+//            if imageSize != .zero {
+//                let image = image.withRenderingMode(.alwaysTemplate)
+//                self.setImage(image.scaleImage(imageSize), for: .normal)
+//            } else {
+//                self.setImage(image.withRenderingMode(.alwaysTemplate), for: .normal)
+//            }
+//        } else {
+//            if imageSize != .zero {
+//                self.setImage(image.scaleImage(imageSize), for: .normal)
+//            } else {
+//                self.setImage(image, for: .normal)
+//            }
+//        }
+//        // 需要刷新
+//        needRefresh = true
+//    }
+//
+//    /**
+//    *  Set image link, read cache synchronously if available
+//    *
+//    *  @param url                      Link
+//    *  @param placeholder        Placeholder image
+//    *  @param syncLoadCache Synchronously read cache
+//    *
+//    */
+//    func setImageUrl(_ url: URL, placeholder: UIImage? = nil, syncLoadCache cache: Bool = true) {
+//        self.setImageUrlString(url.absoluteString, placeholder: placeholder, syncLoadCache: cache)
+//    }
+//
+//    /**
+//    *  Set image link, read from cache if available
+//    *
+//    *  @param urlString             link string
+//    *  @param placeholder        default image
+//    *  @param syncLoadCache whether to read cache synchronously
+//    *
+//    */
+//    func setImageUrlString(_ urlString: String, placeholder: UIImage? = nil, syncLoadCache cache: Bool = true) {
+//        if urlString.count == 0 {
+//            self._setImage(placeholder)
+//            self.lastURL = ""
+//            didGetError?(self, herr(kDataFormatErrorCode, desc: "url = \(urlString)"))
+//            return
+//        }
+//
+//        if urlString.hasPrefix("http") == false {
+//            let image = UIImage(named: urlString)
+//            self._setImage(image)
+//            self.imageView?.alpha = 1.0
+//            didGetImage?(self, self.imageView?.image)
+//            return
+//        }
+//
+//        if self.imageView?.image != nil, lastURL.isEqual(urlString) {
+//            self.imageView?.alpha = 1.0
+//            didGetImage?(self, self.imageView?.image)
+//            return
+//        }
+//
+//        if self.imageView?.image == nil, placeholder == nil {
+//            self.imageView?.alpha = 0
+//        }
+//
+//        self._setImage(nil)
+//        self.lastURL = ""
+//        
+//        guard let url = URL(string: urlString) else {
+//            return
+//        }
+//
+//        if cache {
+//            KingfisherManager.shared.cache.retrieveImage(forKey: urlString) { result in
+//                DispatchQueue.main.async {
+//                    switch result {
+//                    case.success(let value):
+//                        if value.image != nil {
+//                            self._setImage(value.image)
+//                            self.imageView?.alpha = 1.0
+//                            self.lastURL = url.absoluteString
+//                            self.didGetImage?(self, value.image)
+//                        }
+//                    case .failure(_): break
+//                    }
+//                }
+//            }
+//        }
+//        if self.imageView?.image == nil {
+//            //self.imageView?.kf.indicatorType = .activity
+//            self.imageView?.kf.setImage(with: url, placeholder: placeholder, options: [.transition(ImageTransition.fade(1))], progressBlock: nil) { result in
+//                DispatchQueue.main.async {
+//                    switch result {
+//                    case .success(let value):
+//                        self._setImage(value.image)
+//                        self.lastURL = url.absoluteString
+//                        if value.cacheType == .none {
+//                            UIView.animate(withDuration: 0.25) {
+//                                self.imageView?.alpha = 1.0
+//                            }
+//                        }else {
+//                            self.imageView?.alpha = 1.0
+//                        }
+//                        self.didGetImage?(self, value.image)
+//                    case .failure(let value):
+//                        self.didGetError?(self, value)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//}
