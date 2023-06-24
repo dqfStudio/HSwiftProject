c7ecfd178213a8f33807f3d23604fc88f56777e4
dqf <897432098@qq.com>
2023-06-24 14:22:07 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/NS-Category/Ext-Category/NSObject+HSwizzleUtil.swift
+++ HSwiftProject/Classes/QFUtil/NS-Category/Ext-Category/NSObject+HSwizzleUtil.swift
@@ -9,72 +9,79 @@
 import UIKit
 
 func HSwizzleClassMethod(_ cls: AnyClass, _ origSEL: Selector, _ overrideSEL: Selector) {
-    let originalMethod: Method = class_getClassMethod(cls, origSEL)!
-    let swizzledMethod: Method = class_getClassMethod(cls, overrideSEL)!
-    
-    let metacls: AnyClass = objc_getMetaClass(NSStringFromClass(cls).cString(using: .utf8)!) as! AnyClass
-    if (class_addMethod(metacls,
-                        origSEL,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod))) {
-        /* swizzing super class method, added if not exist */
-        class_replaceMethod(metacls,
-                            overrideSEL,
-                            method_getImplementation(originalMethod),
-                            method_getTypeEncoding(originalMethod))
-        
-    }else {
-        /* swizzleMethod maybe belong to super */
-        class_replaceMethod(metacls,
-                            overrideSEL,
-                            class_replaceMethod(metacls,
-                                                origSEL,
-                                                method_getImplementation(swizzledMethod),
-                                                method_getTypeEncoding(swizzledMethod))!,
-                            method_getTypeEncoding(originalMethod))
+    if let originalMethod = class_getClassMethod(cls, origSEL),
+        let swizzledMethod = class_getClassMethod(cls, overrideSEL),
+        let classString = NSStringFromClass(cls).cString(using: .utf8) {
+        let metacls: AnyClass = objc_getMetaClass(classString) as! AnyClass
+        if (class_addMethod(metacls,
+                            origSEL,
+                            method_getImplementation(swizzledMethod),
+                            method_getTypeEncoding(swizzledMethod))) {
+            /* swizzing super class method, added if not exist */
+            class_replaceMethod(metacls,
+                                overrideSEL,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod))
+            
+        }else {
+            /* swizzleMethod maybe belong to super */
+            if let classMethod = class_replaceMethod(metacls,
+                                                     origSEL,
+                                                     method_getImplementation(swizzledMethod),
+                                                     method_getTypeEncoding(swizzledMethod)) {
+                class_replaceMethod(metacls,
+                                    overrideSEL,
+                                    classMethod,
+                                    method_getTypeEncoding(originalMethod))
+            }
+        }
     }
 }
 
 func HSwizzleInstanceMethod(_ cls: AnyClass, _ origSEL: Selector, _ overrideSEL: Selector) {
     /* if current class not exist selector, then get super*/
-    let originalMethod: Method = class_getClassMethod(cls, origSEL)!
-    let swizzledMethod: Method = class_getClassMethod(cls, overrideSEL)!
-    
-    /* add selector if not exist, implement append with method */
-    if (class_addMethod(cls,
-                        origSEL,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod))) {
-        /* replace class instance method, added if selector not exist */
-        /* for class cluster , it always add new selector here */
-        class_replaceMethod(cls,
-                            overrideSEL,
-                            method_getImplementation(originalMethod),
-                            method_getTypeEncoding(originalMethod))
-        
-    }else {
-        /* swizzleMethod maybe belong to super */
-        class_replaceMethod(cls,
-                            overrideSEL,
-                            class_replaceMethod(cls,
-                                                origSEL,
-                                                method_getImplementation(swizzledMethod),
-                                                method_getTypeEncoding(swizzledMethod))!,
-                            method_getTypeEncoding(originalMethod))
+    if let originalMethod = class_getClassMethod(cls, origSEL),
+        let swizzledMethod = class_getClassMethod(cls, overrideSEL) {
+        /* add selector if not exist, implement append with method */
+        if (class_addMethod(cls,
+                            origSEL,
+                            method_getImplementation(swizzledMethod),
+                            method_getTypeEncoding(swizzledMethod))) {
+            /* replace class instance method, added if selector not exist */
+            /* for class cluster , it always add new selector here */
+            class_replaceMethod(cls,
+                                overrideSEL,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod))
+            
+        }else {
+            /* swizzleMethod maybe belong to super */
+            if let classMethod = class_replaceMethod(cls,
+                                                     origSEL,
+                                                     method_getImplementation(swizzledMethod),
+                                                     method_getTypeEncoding(swizzledMethod)) {
+                class_replaceMethod(cls,
+                                    overrideSEL,
+                                    classMethod,
+                                    method_getTypeEncoding(originalMethod))
+            }
+        }
     }
 }
 
 func HSwizzleClassMethodNames(_ classNames: NSArray, _ origSEL: Selector, _ overrideSEL: Selector) {
-    if classNames.count == 0 { return }
-    for className in classNames {
-        HSwizzleClassMethod(NSClassFromString(className as! String)!, origSEL, overrideSEL)
+    classNames.forEach { className in
+        if let classString = NSClassFromString(className as! String) {
+            HSwizzleClassMethod(classString, origSEL, overrideSEL)
+        }
     }
 }
 
 func HSwizzleInstanceMethodNames(_ classNames: NSArray, _ origSEL: Selector, _ overrideSEL: Selector) {
-    if classNames.count == 0 { return }
-    for className in classNames {
-        HSwizzleClassMethod(NSClassFromString(className as! String)!, origSEL, overrideSEL)
+    classNames.forEach { className in
+        if let classString = NSClassFromString(className as! String) {
+            HSwizzleClassMethod(classString, origSEL, overrideSEL)
+        }
     }
 }
 
