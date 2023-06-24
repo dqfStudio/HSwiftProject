bea8dfee4cd091d1826b73eaa361d90f32f38a21
dqf <897432098@qq.com>
2023-06-24 13:50:59 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HSafeUtil/NSObject+HForwarding.swift
+++ HSwiftProject/Classes/QFUtil/HSafeUtil/NSObject+HForwarding.swift
@@ -11,58 +11,58 @@ import UIKit
 extension NSObjectProtocol {
 
     @discardableResult
-    func performWithRetainedValue(_ aSelector: Selector!, withPre pre: String!) -> AnyObject? {
+    func performWithRetainedValue(_ aSelector: Selector, withPre pre: String) -> AnyObject? {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         return self.perform(selector).takeRetainedValue()
     }
     @discardableResult
-    func performWithUnretainedValue(_ aSelector: Selector!, withPre pre: String!) -> AnyObject? {
+    func performWithUnretainedValue(_ aSelector: Selector, withPre pre: String) -> AnyObject? {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         return self.perform(selector).takeUnretainedValue()
     }
     @discardableResult
-    func performWithUnretainedValue(_ aSelector: Selector!) -> AnyObject? {
+    func performWithUnretainedValue(_ aSelector: Selector) -> AnyObject? {
         return self.perform(aSelector).takeUnretainedValue()
     }
 
     
     @discardableResult
-    func performWithRetainedValue(_ aSelector: Selector!, with object: Any!, withPre pre: String!) -> AnyObject? {
+    func performWithRetainedValue(_ aSelector: Selector, with object: Any, withPre pre: String) -> AnyObject? {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         return self.perform(selector, with: object).takeRetainedValue()
     }
     @discardableResult
-    func performWithUnretainedValue(_ aSelector: Selector!, with object: Any!, withPre pre: String!) -> AnyObject? {
+    func performWithUnretainedValue(_ aSelector: Selector, with object: Any, withPre pre: String) -> AnyObject? {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         return self.perform(selector, with: object).takeUnretainedValue()
     }
     @discardableResult
-    func performWithUnretainedValue(_ aSelector: Selector!, with object: Any!) -> AnyObject? {
+    func performWithUnretainedValue(_ aSelector: Selector, with object: Any) -> AnyObject? {
         return self.perform(aSelector, with: object).takeUnretainedValue()
     }
 
     
     @discardableResult
-    func performWithRetainedValue(_ aSelector: Selector!, with object1: Any!, with object2: Any!, withPre pre: String!) -> AnyObject? {
+    func performWithRetainedValue(_ aSelector: Selector, with object1: Any, with object2: Any, withPre pre: String) -> AnyObject? {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         return self.perform(selector, with: object1, with: object2).takeRetainedValue()
     }
     @discardableResult
-    func performWithUnretainedValue(_ aSelector: Selector!, with object1: Any!, with object2: Any!, withPre pre: String!) -> AnyObject? {
+    func performWithUnretainedValue(_ aSelector: Selector, with object1: Any, with object2: Any, withPre pre: String) -> AnyObject? {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         return self.perform(selector, with: object1, with: object2).takeUnretainedValue()
     }
     
     
-    func perform(_ aSelector: Selector!, withPre pre: String!) {
+    func perform(_ aSelector: Selector, withPre pre: String) {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         self.perform(selector)
     }
-    func perform(_ aSelector: Selector!, with object: Any!, withPre pre: String!) {
+    func perform(_ aSelector: Selector, with object: Any, withPre pre: String) {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         self.perform(selector, with: object)
     }
-    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!, withPre pre: String!) {
+    func perform(_ aSelector: Selector, with object1: Any, with object2: Any, withPre pre: String) {
         let selector = NSSelectorFromString(pre + NSStringFromSelector(aSelector))
         self.perform(selector, with: object1, with: object2)
     }
