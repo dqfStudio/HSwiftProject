c70b63bbb28d24fee69d77261d6110ef3aa9b287
dqf <897432098@qq.com>
2023-06-24 13:35:32 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HTextLoopView/HTupleViewTextLoopApex.swift
+++ HSwiftProject/Classes/QFUtil/HTextLoopView/HTupleViewTextLoopApex.swift
@@ -12,38 +12,28 @@ typealias HTupleViewTextLoopApexBlock = (_ selectString: NSString, _ index: Int)
 
 class HTupleViewTextLoopApex: HTupleBaseApex {
 
-    private var _contentArr: NSArray?
     var contentArr: NSArray? {
-        get {
-            return _contentArr
-        }
-        set {
-            if _contentArr != newValue {
-                _contentArr = nil
-                _contentArr = newValue
-                if _contentArr!.count > 0 {
-                    self.layoutView.addSubview(self.textLoopView)
-                }
+        didSet {
+            if let arr = contentArr, arr.count > 0, let textLoopView = self.textLoopView, textLoopView.superview == nil {
+                self.layoutView.addSubview(textLoopView)
             }
         }
     }
     var selectedBlock: HTupleViewTextLoopApexBlock?
     
     private var _textLoopView: HTextLoopView?
-    var textLoopView: HTextLoopView {
-        if _textLoopView == nil {
-            _textLoopView = HTextLoopView.textLoopViewWithFrame(self.bounds, self.contentArr!, 2.0) { (selectString, index) in
-                if self.selectedBlock != nil {
-                    self.selectedBlock!(selectString, index)
-                }
+    var textLoopView: HTextLoopView? {
+        if let arr = contentArr, arr.count > 0, _textLoopView == nil {
+            _textLoopView = HTextLoopView.textLoopViewWithFrame(self.bounds, arr, 2.0) { (selectString, index) in
+                self.selectedBlock?(selectString, index)
             }
         }
-        return _textLoopView!
+        return _textLoopView
     }
     
     override func relayoutSubviews() {
-        if self.contentArr!.count > 0 {
-            HLayoutTupleApex(self.textLoopView)
+        if let arr = contentArr, arr.count > 0, let textLoopView = self.textLoopView {
+            HLayoutTupleApex(textLoopView)
         }
     }
 
