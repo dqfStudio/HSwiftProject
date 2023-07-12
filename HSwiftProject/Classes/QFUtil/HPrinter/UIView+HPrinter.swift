d671d7afa1ccbcda6bd9f8f55cb016e1fd2d0567
dqf <897432098@qq.com>
2023-07-12 15:28:48 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HPrinter/UIView+HPrinter.swift
+++ HSwiftProject/Classes/QFUtil/HPrinter/UIView+HPrinter.swift
@@ -33,53 +33,23 @@ extension UIView {
     }
     
     private func logAction() {
+        
         if self.isSystemClass(self.classForCoder) == false {
             print("HPrinting-->className:\(self.className)\n")
         }
-        let loginfo = self.logInfo()
-        if !loginfo.isEmpty {
+        
+        if let loginfo = self.logInfo(), !loginfo.isEmpty {
             print("HPrinting-->loginfo:\(loginfo)\n")
         }
-        if self.isKind(of: UILabel.self) {
-            let label: UILabel = self as! UILabel
-            if let text = label.text, !text.isEmpty {
-                print("HPrinting-->label.text:\(text)\n")
-            }
+
+        if let label = self as? UILabel, let text = label.text, !text.isEmpty {
+            print("HPrinting-->label.text:\(text)\n")
         }
-        else if self.isKind(of: UITextView.self) {
-            let textView: UITextView = self as! UITextView
-            if let text = textView.text, !text.isEmpty {
-                print("HPrinting-->textView.text:\(text)\n")
-            }
+        else if let textView = self as? UITextView, let text = textView.text, !text.isEmpty {
+            print("HPrinting-->textView.text:\(text)\n")
         }
-        else if self.isKind(of: UIControl.self) {
-            if self.isKind(of: UIButton.self) {
-                let btn: UIButton = self as! UIButton
-                if let text = btn.titleLabel?.text, !text.isEmpty {
-                    print("HPrinting-->button.text:\(text)\n")
-                }
-//                if btn.allTargets.count > 0 {
-//                    let objc: AnyHashable = btn.allTargets.first!
-//                    print("HPrinting-->button.targets:\(objc.description)\n")
-//                }
-                if let identifier = btn.imageView?.image?.accessibilityIdentifier, !identifier.isEmpty {
-                    print("HPrinting-->button.image.name:\(identifier)\n")
-                    return
-                }
-            }else if self.isKind(of: UIControl.self) {
-//                let control: UIControl = self as! UIControl
-//                if control.allTargets.count > 0 {
-//                    let objc: AnyHashable = control.allTargets.first!
-//                    print("HPrinting-->control.targets:\(objc.description)\n")
-//                }
-            }
-        }
-        else if self.isKind(of: UIImageView.self) {
-            let imageView: UIImageView = self as! UIImageView
-            if let identifier = imageView.image?.accessibilityIdentifier, !identifier.isEmpty {
-                print("HPrinting-->imageView.image.name:\(identifier)\n")
-            }
-            return
+        else if let btn = self as? UIButton, let text = btn.titleLabel?.text, !text.isEmpty {
+            print("HPrinting-->button.text:\(text)\n")
         }
         
         if let superview = self.superview, self.isSystemClass(superview.classForCoder) == false {
@@ -91,65 +61,33 @@ extension UIView {
         }
         
         self.logVC()
+        
     }
     
     private func logVC() {
-        
-        var next = self.next
-        var controller: UIViewController?
-
-        while next?.isKind(of: UIViewController.self) == false {
-            next = next?.next
-            if next == nil {
-                break
-            }
-        }
-        if next?.isKind(of: UIViewController.self) == true {
-            controller = next as? UIViewController
-            print("HPrinting-->viewController:\(controller!.className)\n")
-        }
+        guard let controller = findViewController() else { return }
+        print("HPrinting-->viewController:\(controller.className)\n")
     }
-    
-    private func logInfo() -> String {
-        let aKey: String = String(format: "%p", self)
-        if HPrinterManager.share.containsObject(aKey) {
-            return HPrinterManager.share.objectForKey(aKey) ?? ""
+
+    private func findViewController() -> UIViewController? {
+        var next = self.next
+        while let current = next {
+            if current is UIViewController {
+                return current as? UIViewController
+            }
+            next = current.next
         }
-        return ""
+        return nil
+    }
+
+    private func logInfo() -> String? {
+        let aKey = String(format: "%p", self)
+        if HPrinterManager.share.containsObject(aKey) {
+            return HPrinterManager.share.objectForKey(aKey)
+        }
+        return nil
     }
     
 #endif
     
 }
-
-//extension UIImage {
-//
-//    @objc class func swizzle() {
-//        Swizzle(UIImage.self) {
-////            #selector(imageNamed:) <-> #selector(myViewDidLoad)
-//            #selector(init?(named _:)) <-> #selector(init?(mynamed _:))
-//
-////            init?(named name: String)
-//        }
-//    }
-//
-//    public /*not inherited*/ convenience init?(named name: String, file: String = #file) {
-////        super.ini
-//    }
-//
-//    @objc private func myViewDidLoad() {
-//        print(#function)
-//        myViewDidLoad()
-//        UIImage(named: "")
-//        UIImage.init(named: <#T##String#>)
-////        UIImage.init(named: <#T##String#>)
-//    }
-//
-//    @objc private func myViewWillAppear(_ animated: Bool) {
-//        print(#function)
-//        myViewWillAppear(animated)
-//    }
-//
-//}
-
-
