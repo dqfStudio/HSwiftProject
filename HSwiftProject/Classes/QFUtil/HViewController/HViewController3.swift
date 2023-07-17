1da565d0ee3ee9001551c83c74d4e4b404c45a17
dqf <897432098@qq.com>
2023-07-17 21:59:12 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HViewController/HViewController3.swift
+++ HSwiftProject/Classes/QFUtil/HViewController/HViewController3.swift
@@ -45,6 +45,7 @@ class HViewController3: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.becomeFirstResponder()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
         if #available(iOS 11.0, *) {
             if let scrollView = self.view as? UIScrollView {
                 scrollView.contentInsetAdjustmentBehavior = .never
