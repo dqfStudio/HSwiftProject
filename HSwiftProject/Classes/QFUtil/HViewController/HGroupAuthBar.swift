aa349574de8495598d309eba7d010b314c5090a3
dqf <897432098@qq.com>
2024-04-17 16:48:43 +0800

    modify some files

---
+++ HSwiftProject/Classes/QFUtil/HViewController/HGroupAuthBar.swift
@@ -0,0 +1,114 @@
+//
+//  HGroupAuthBar.swift
+//  HSwiftProject
+//
+//  Created by owner on 2024/4/17.
+//  Copyright © 2024 wind. All rights reserved.
+//
+
+import UIKit
+
+typealias HGroupAuthBarBlock = (_ auth: String) -> Void
+
+class HGroupAuthBar: UIStackView, HTupleViewDelegate {
+
+    var nameString: String?
+    var nameColor = UIColor.white
+    var nameFont = UIFont.font(ofSize: 14, weight: .regular)
+    
+    var authString: String = "" {
+        didSet {
+            if authString != oldValue {
+                auths = sortAuth(authString)
+                tupleView.reloadTupleData()
+            }
+        }
+    }
+    private var auths: [String] = []
+    private var authSize: CGFloat = 17.0
+    var selectBlock: HGroupAuthBarBlock?
+    
+    private lazy var tupleView: HTupleView = {
+        let tupleView = HTupleView(frame: .zero)
+        tupleView.backgroundColor = .clear
+        tupleView.isScrollEnabled = false
+        tupleView.disableBounce()
+        return tupleView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .clear
+        self.spacing = 4.0
+        self.tupleView.delegate = self
+        self.addArrangedSubview(self.tupleView)
+    }
+
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 将授权字符串按照GM、VIP、POP、OTC、UGC的顺序返回
+    private func sortAuth(_ auth: String) -> [String] {
+        var authString = auth.replacingOccurrences(of: " ", with: "")
+        authString = authString.replacingOccurrences(of: "，", with: ",")
+        guard !authString.isEmpty else { return [] }
+        let items = authString.components(separatedBy: ",")
+        let order = ["GM", "VIP", "POP", "OTC", "UGC"]
+        return items.sorted { (first, second) -> Bool in
+            guard let firstIndex = order.firstIndex(of: first), let secondIndex = order.firstIndex(of: second) else { return false }
+            return firstIndex < secondIndex
+        }
+    }
+    
+}
+
+extension HGroupAuthBar {
+    
+    func numberOfItemsInSection(_ section: Any) -> Any {
+        return self.auths.count + 1
+    }
+    
+    func sizeForItemAtIndexPath(_ indexPath: IndexPath) -> Any {
+        if indexPath.row == 0 {
+            // 名称字符长度
+            var nameWidth = self.nameString?.widthWithFont(self.nameFont,
+                                                           constrainedToHeight: self.height) ?? 1.0
+            nameWidth = ceil(nameWidth) //向上取整
+            // 授权字符长度
+            var authWidth = self.width - (self.spacing + self.authSize) * CGFloat(self.auths.count)
+            authWidth = max(authWidth, 0) //取最大值
+            // 取名称与授权字符长度的最小值
+            nameWidth = min(nameWidth, authWidth) //取最小值
+            return nameWidth
+        }else {
+            return CGSize(width: self.authSize, height: self.authSize)
+        }
+    }
+    
+    func minimumInteritemSpacingForSectionAt(_ section: Any) -> Any {
+        return self.spacing
+    }
+    
+    func tupleItem(_ itemBlock: Any, atIndexPath indexPath: IndexPath) {
+        let itemBlock = itemBlock as! HTupleItem
+        if indexPath.row == 0 {
+            let cell = itemBlock(HTupleLabelCell.self, nil, true) as! HTupleLabelCell
+            cell.label.textColor = self.nameColor
+            cell.label.font = self.nameFont
+            cell.label.text = self.nameString
+        }else {
+            let cell = itemBlock(HTupleImageCell.self, nil, true) as! HTupleImageCell
+            if indexPath.row <= self.auths.count {
+                let authIndex = indexPath.row - 1
+                let auth = self.auths[authIndex]
+                //cell.imageView.setImage(WithName: auth)
+                cell.selectBlock = { [weak self] in
+                    self?.selectBlock?(auth)
+                }
+            }
+        }
+    }
+    
+}
