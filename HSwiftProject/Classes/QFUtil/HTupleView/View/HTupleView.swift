588cea860ae2ce57bd3cbeb3358d2cacb8a21d75
dqf <897432098@qq.com>
2024-04-24 10:07:24 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HTupleView/View/HTupleView.swift
+++ HSwiftProject/Classes/QFUtil/HTupleView/View/HTupleView.swift
@@ -24,7 +24,7 @@ enum HTupleStatus: Int {
 }
 
 enum HTupleAlign {
-    case zero // 垂直居上，水平居左
+    case `default` // 垂直居上，水平居左
     case center // 垂直居中，水平居中
     case top(CGFloat) // 垂直距离顶部的距离，水平居中
     case ratio(CGFloat) // 垂直距离顶部的比例，水平居中
@@ -202,12 +202,12 @@ class HTupleView: UICollectionView, UICollectionViewDelegate, UICollectionViewDa
     var tupleStatus: HTupleStatus = .delegate
     
     // tuple align
-    var tupleAlign: HTupleAlign = .zero {
+    var tupleAlign: HTupleAlign = .default {
         didSet {
             let cntSize = self.contentSize
             let cntInset = self.contentInset
             switch tupleAlign {
-            case .zero:
+            case .default:
                 self.contentInset = UIEdgeInsets.zero
             case .center:
                 let originX = (self.width - cntSize.width) / 2
