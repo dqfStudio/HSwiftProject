9c5423742b61b7034f60284669f1d5f236b80f1f
dqf <897432098@qq.com>
2025-04-06 00:54:41 +0800

    modify some files

--- HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HGlobalObserver.swift
+++ HSwiftProject/Classes/QFUtil/HTupleView/Cells/HTupleCommon/HGlobalObserver.swift
@@ -8,50 +8,58 @@
 
 import Foundation
 
-/// 定义观察者协议
+// 定义观察者协议
 @objc protocol HGlobalObserverProtocol: NSObjectProtocol {
     @objc
     optional func refreshColl()
-    
     @objc
     optional func releaseColl()
 }
 
-/// 定义观察者
-class HGlobalObserver: NSObject {
+// 定义枚举来表示协议中的方法
+enum HGlobalObserverType {
+    case refreshColl
+    case releaseColl
 
-    static let share = HGlobalObserver()
-    private var hashObjects = NSHashTable<HGlobalObserverProtocol>.weakObjects()
-
-    func addObserver(_ anObserver: HGlobalObserverProtocol?) {
-        if let anObserver = anObserver, !self.hashObjects.contains(anObserver) {
-            self.hashObjects.add(anObserver)
+    var selector: Selector {
+        switch self {
+        case .refreshColl:
+            return #selector(HGlobalObserverProtocol.refreshColl)
+        case .releaseColl:
+            return #selector(HGlobalObserverProtocol.releaseColl)
         }
     }
-    
-    func perform(key: String, with object1: String? = nil, with object2: String? = nil, _ completion: @escaping () -> Void) {
+}
+
+// 定义观察者
+class HGlobalObserver {
+    static let shared = HGlobalObserver()
+    private var observers = NSHashTable<AnyObject>.weakObjects()
+
+    func addObserver(_ observer: HGlobalObserverProtocol) {
+        guard !observers.contains(observer as AnyObject) else { return }
+        observers.add(observer as AnyObject)
+    }
+
+    private func callSelector(_ observer: AnyObject, selector: Selector, args: [Any]) {
+        switch args.count {
+        case 0: _ = observer.perform(selector)
+        case 1: _ = observer.perform(selector, with: args[0])
+        case 2: _ = observer.perform(selector, with: args[0], with: args[1])
+        default: break
+        }
+    }
+
+    func perform(_ observerType: HGlobalObserverType, with object1: Any? = nil, with object2: Any? = nil, completion: @escaping () -> Void) {
         Task {
-            let selector = NSSelectorFromString(key)
-            let objects = self.hashObjects.allObjects.reversed()
-            for object in objects {
-                if object.responds(to: selector) {
-                    await MainActor.run {
-                        switch (object1, object2) {
-                        case let (objc1?, objc2?):
-                            _ = object.perform(selector, with: objc1, with: objc2)
-                        case let (objc1?, nil):
-                            _ = object.perform(selector, with: objc1)
-                        case let (nil, objc2?):
-                            _ = object.perform(selector, with: objc2)
-                        case (nil, nil):
-                            _ = object.perform(selector)
-                        }
-                    }
+            let selector = observerType.selector
+            let args = [object1, object2].compactMap { $0 }
+            for observer in observers.allObjects.reversed() where observer.responds(to: selector) {
+                await MainActor.run {
+                    callSelector(observer, selector: selector, args: args)
                 }
             }
-            await MainActor.run {
-                completion()
-            }
+            await MainActor.run { completion() }
         }
     }
 }
